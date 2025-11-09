af7c459b2406f8a7d9ddd33a352d055176fe7cb9
Szalay Gergő <gergosz9@gmail.com>
2025-11-09 14:24:47 +0100

    feat: add multiple qr code tracking with payload and visuals

--- Assets/Scripts/QRTracking/QRCodeTracker.cs
+++ Assets/Scripts/QRTracking/QRCodeTracker.cs
@@ -1,4 +1,7 @@
 using Meta.XR.MRUtilityKit;
+using System.Collections.Generic;
+using TMPro;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class QRCodeTracker : MonoBehaviour
@@ -7,6 +10,8 @@ public class QRCodeTracker : MonoBehaviour
     private MRUK MRUtilityKit;
     [SerializeField]
     private GameObject QRCodePrefab;
+
+    private Dictionary<MRUKTrackable, GameObject> trackedQRCodes = new Dictionary<MRUKTrackable, GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,17 +24,32 @@ public class QRCodeTracker : MonoBehaviour
         MRUtilityKit.SceneSettings.TrackableAdded.AddListener(OnTrackableAdded);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        foreach (var trackedQRCode in trackedQRCodes)
+        {
+            trackedQRCode.Value.transform.position = trackedQRCode.Key.transform.position;
+            trackedQRCode.Value.transform.rotation = trackedQRCode.Key.transform.rotation;
+        }
     }
 
     private void OnTrackableAdded(MRUKTrackable trackable)
     {
         if (trackable.TrackableType == OVRAnchor.TrackableType.QRCode)
         {
-            Instantiate(QRCodePrefab, trackable.transform.position, trackable.transform.rotation);
+            if (!trackedQRCodes.ContainsKey(trackable))
+            {
+                GameObject qrCodeObject = Instantiate(QRCodePrefab, trackable.transform.position, trackable.transform.rotation);
+                trackedQRCodes.Add(trackable, qrCodeObject);
+
+                string qrPayload = trackable.MarkerPayloadString;
+                Transform canvasChild = qrCodeObject.transform.GetChild(0);
+                TextMeshProUGUI tmpComponent = canvasChild.GetComponentInChildren<TextMeshProUGUI>();
+                if (tmpComponent != null)
+                {
+                    tmpComponent.text = qrPayload;
+                }
+            }
         }
     }
 }
